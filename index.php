34ee0865d161658d60d9db8705955400b5eb053d
havinnaz-ozer <havinnazozer@gmail.com>
2026-05-09 15:27:35 +0300

    eksik satırlar eklendi

--- index.php
+++ index.php
@@ -188,4 +188,14 @@ if ($rez_sonuc) {
 
             <div class="step-card">
                 <div class="step-icon">
-                    <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1
+                    <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
+                </div>
+                <h3>Rezerve Et</h3>
+                <p>Rezervasyonunu onayla.</p>
+            </div>
+
+        </div>
+    </section>
+
+</body>
+</html>
